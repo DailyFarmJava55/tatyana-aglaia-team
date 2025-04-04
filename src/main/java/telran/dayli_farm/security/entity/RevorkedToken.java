77f0db95ef76f60249b22340ba9f54a04a1fcc78
TatyanaNikitenko <tatiananikitenko8@gmail.com>
2025-04-04 12:21:53 +0300

    update
    add dockerfile and deleted JPQL console

--- src/main/java/telran/dayli_farm/security/entity/RevorkedToken.java
+++ src/main/java/telran/dayli_farm/security/entity/RevorkedToken.java
@@ -9,10 +9,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
 @Table(name = "token_blacklist")
+@NoArgsConstructor
 public class RevorkedToken {
 	@UuidGenerator
 	@Id
